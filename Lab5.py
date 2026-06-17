b74eb6fc54b05e67e607feaa58aded3e1460902f
victory-pixel <victoryojok2004@gmail.com>
2026-06-17 14:25:17 +0300

    Classwork

---
+++ Lab5.py
@@ -0,0 +1,16 @@
+#1Advanced Functions
+#2Lambda functions
+'''
+Small annonymous functions that have any number of arguments but only one expression. 
+They're defined by the keyword Lambda.
+Syntax:
+Lambda arguments: expression
+
+Key X-tics
+1. no name
+2. single expression
+3.Can be used where function objects are required
+'''
+#3Recursive functions
+#4Built-in functions
+#5Function - based project
---
+++ Morning/modules.py
@@ -0,0 +1,20 @@
+#Modules
+#Help organise code into reusable files
+#They're both inbuilt and external
+# Inbuilt ones can be accessed using the import keyword
+#Why we need them
+#Reuse code
+#Keep code organised and clean
+#avoid clutter
+#Boost productivity
+#E.g
+import math
+pie = math.pi
+print(pie)
+
+
+#External Modules
+#Steps
+#!. Install the module using pip install module_name
+#Examples are pandas, nampy
+#2.
